438821a963794b6b7a7a699c7b21518fd72e1059
Alex Rocha <rocha.amc@gmail.com>
2022-10-30 17:34:29 -0700

    Add get balance method

--- BankAccount.py
+++ BankAccount.py
@@ -1,27 +1,32 @@
 class BankAccount:
-    def __init__(self, full_name, account_number, balance = 0):
+    def __init__(self, full_name, account_number, balance=0):
         self.full_name = full_name
         self.account_number = account_number
         self.balance = balance
 
     def deposit(self, amount):
         self.balance += amount
-        print(f'Amount deposited: ${amount} new balance: ${self.balance}')
+        print(f"Amount deposited: ${amount} new balance: ${self.balance}")
 
     def withdraw(self, amount):
-        if(self.balance - amount < 0):
-            print('Insufficient funds.')
+        if self.balance - amount < 0:
+            print("Insufficient funds.")
             self.balance -= 10
         else:
             self.balance -= amount
-            print(f'Amount withdrawn: ${amount} new balance: ${self.balance}')
+            print(f"Amount withdrawn: ${amount} new balance: ${self.balance}")
+
+    def get_balance(self):
+        print(f"Your balance is ${self.balance}.")
+        return self.balance
 
 
-
-alex_account = BankAccount('Alex Rocha', 43536373)
+alex_account = BankAccount("Alex Rocha", 43536373)
 
 alex_account.deposit(1000)
 alex_account.deposit(500)
 
 alex_account.withdraw(1400)
 alex_account.withdraw(200)
+
+alex_account.get_balance()
