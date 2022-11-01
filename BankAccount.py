66dd9e6f95b1567a370b905fff62353a970f4c9c
Alex Rocha <rocha.amc@gmail.com>
2022-10-31 18:09:26 -0700

    Add account number generator

--- BankAccount.py
+++ BankAccount.py
@@ -1,15 +1,27 @@
+import random
+
+
 class BankAccount:
-    def __init__(self, full_name, account_number, balance=0):
+    def __init__(self, full_name, balance=0):
         self.full_name = full_name
-        self.account_number = account_number
         self.balance = balance
+        self.account_number = self.__account_number_generator()
+
+    def __account_number_generator(self):
+        number = [str(random.randint(1, 9))]
+        iteration = 1
+        while iteration < 8:
+            number.append(str(random.randint(0, 9)))
+            iteration += 1
+        number = "".join(number)
+        return number
 
     def deposit(self, amount):
         self.balance += amount
         print(f"Amount deposited: ${amount:.2f} new balance: ${self.balance:.2f}")
 
     def withdraw(self, amount):
-        if self.balance - amount < 0:
+        if self.balance < amount:
             print("Insufficient funds.")
             self.balance -= 10
         else:
@@ -32,9 +44,10 @@ class BankAccount:
             f"\n{self.full_name}\nAccount No.: {censored_account_number}\nBalance: ${self.balance:.2f}\n"
         )
 
-huey = BankAccount("Hubert Duck", 1029384756)
-dewey = BankAccount("Dewford Dingus Duck", 1874609235)
-louie = BankAccount("Llewellyn Duck", 1627384509)
+
+huey = BankAccount("Hubert Duck")
+dewey = BankAccount("Dewford Dingus Duck")
+louie = BankAccount("Llewellyn Duck")
 
 huey.deposit(400000)
 huey.print_statement()
@@ -43,7 +56,7 @@ huey.print_statement()
 huey.withdraw(150)
 huey.print_statement()
 
-print('===========\n')
+print("===========\n")
 
 dewey.deposit(400)
 dewey.print_statement()
@@ -54,7 +67,7 @@ dewey.print_statement()
 dewey.withdraw(150)
 dewey.print_statement()
 
-print('===========\n')
+print("===========\n")
 
 louie.deposit(1000)
 louie.print_statement()
